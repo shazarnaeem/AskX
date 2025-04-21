6916bf41b2056ab917e8ba3143595394532526d7
shazarnaeem <shazarnaeem5.com>
2025-04-22 02:06:19 +0500

    fix: add Next.js pages directory and configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatInterface from '../components/ChatInterface';
+
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <ChatInterface />
+    </div>
+  );
+};
+
+export default Home; 
