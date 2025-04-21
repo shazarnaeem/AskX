eab6b6507145d3e1abe15832a63975ace5973791
shazarnaeem <shazarnaeem5.com>
2025-04-22 02:13:15 +0500

    revert: remove Next.js and restore original React setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ChatInterface from '../components/ChatInterface';
+import ChatInterface from './components/ChatInterface';
 
-const Home: React.FC = () => {
+function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <ChatInterface />
     </div>
   );
-};
+}
 
-export default Home; 
+export default App; 
